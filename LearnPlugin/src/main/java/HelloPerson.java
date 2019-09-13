91fbd099bbbc10e4ae4b9296314c005d59a6d8d0
1173171968@qq.com <1173171968@qq.com>
2019-09-13 18:17:25 +0800

    Initial commit

---
+++ LearnPlugin/src/main/java/HelloPerson.java
@@ -0,0 +1,7 @@
+public class HelloPerson {
+
+    public static void main(String[] args) {
+        Person person = new Person(111, "ban", "yanan");
+        System.out.println(person.toString());
+    }
+}
---
+++ myGrovyPlug/src/myOutPlug.groovy
@@ -0,0 +1,6 @@
+import org.apache.tools.ant.Project
+
+class myOutPlug implements Plugin<Project> {
+
+
+}
